b84a6a7e5f09c8c2397a03d03473f6e5a76354df
Andy Chung <andy6756@gmail.com>
2022-01-24 09:29:11 +0800

    Pass data to a parent component (Fail)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { products } from './products';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -7,6 +8,11 @@ import { HelloWorldInterpolationComponent } from './hello-world-interpolation/he
 import { HelloWorldBindingsComponent } from './hello-world-bindings/hello-world-bindings.component';
 import { HelloWorldNgIfComponent } from './hello-world-ng-if/hello-world-ng-if.component';
 import { HelloWorldDiComponent } from './hello-world-di/hello-world-di.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -15,10 +21,17 @@ import { HelloWorldDiComponent } from './hello-world-di/hello-world-di.component
     HelloWorldInterpolationComponent,
     HelloWorldBindingsComponent,
     HelloWorldNgIfComponent,
-    HelloWorldDiComponent
+    HelloWorldDiComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductAlertsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {path: '', component: ProductListComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
