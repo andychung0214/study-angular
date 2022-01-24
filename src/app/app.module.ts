9861ea0ad33cfcd7f097b65afdb8138e0b2c7311
Andy Chung <andy6756@gmail.com>
2022-01-24 15:46:41 +0800

    嘹解Angular-元件-概覽

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { CartComponent } from './cart/cart.component';
 
 import { HttpClientModule } from "@angular/common/http";
+import { ComponentOverviewComponent } from './component-overview/component-overview.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { HttpClientModule } from "@angular/common/http";
     ProductAlertsComponent,
     ProductDetailsComponent,
     CartComponent,
-    ShippingComponent
+    ShippingComponent,
+    ComponentOverviewComponent
   ],
   imports: [
     BrowserModule,
